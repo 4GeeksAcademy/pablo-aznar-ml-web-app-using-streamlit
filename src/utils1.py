9115695471358f5e9faaddac333bb15ee1d809dc
Pablo-Aznar <paznaraniorte@gmail.com>
2025-08-03 22:54:08 +0000

    Corregido utils1.py y añadido modelo y vectorizador

--- src/utils1.py
+++ src/utils1.py
@@ -1,32 +1,32 @@
 import re
-from nltk import download
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-download("wordnet")
-download("stopwords")
+# Comprueba y descarga recursos solo si faltan
+try:
+    nltk.data.find("corpora/wordnet")
+except LookupError:
+    nltk.download("wordnet")
+
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords")
 
 lemmatizer = WordNetLemmatizer()
 stop_words = stopwords.words("english")
 
 def preprocess_text(text):
     text = text.lower()
-    # Reemplazar extensiones por espacios
     text = re.sub(r'https?://|www\.|\.com|\.html|\.pdf|\.gov|\.net|\.org|\.edu|\.php|\.aspx', ' ', text)
-
-    # Eliminar cualquier caracter que no sea una letra (a-z) o un espacio en blanco ( )
     text = re.sub(r'[^a-z ]', " ", text)
-
-    # Eliminar espacios en blanco
     text = re.sub(r'\s+[a-zA-Z]\s+', " ", text)
     text = re.sub(r'\^[a-zA-Z]\s+', " ", text)
-
-    # Reducir espacios en blanco múltiples a uno único
     text = re.sub(r'\s+', " ", text)
-
     return text.split()
 
-def lemmatize_text(words, lemmatizer = lemmatizer):
+def lemmatize_text(words, lemmatizer=lemmatizer):
     tokens = [lemmatizer.lemmatize(word) for word in words]
     tokens = [word for word in tokens if word not in stop_words]
     tokens = [word for word in tokens if len(word) > 3]
